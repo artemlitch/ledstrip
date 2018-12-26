c87cdd91dfa3c62569890baa0c28518d19f2408a
Artem's Pi <artem@rasppi.com>
2018-12-26 23:25:24 +0000

    nint

---
+++ index.js
@@ -0,0 +1,18 @@
+var Gpio = require('pigpio').Gpio;
+console.log('Define each color from RGB Strip light.');
+//For each color you need to specify on what GPIO pin is connected and that we will use in mode OUTPUT
+var ledRed = new Gpio(23, {mode: Gpio.OUTPUT});
+var ledGreen = new Gpio(24, {mode: Gpio.OUTPUT});
+var ledBlue = new Gpio(25, {mode: Gpio.OUTPUT});
+console.log('Power up the red color.');
+//You can set a brightness value between 0 and 255 
+//where 0 is off and 255 is maximum brightness
+ledRed.pwmWrite(255);
+console.log("Let's start other colors too");
+//Stop the lights after 5 seconds
+setTimeout(function(){
+    console.log('Stop all colors after 5 seconds');
+    ledRed.pwmWrite(0);
+    ledGreen.pwmWrite(0);  
+    ledBlue.pwmWrite(0);
+}, 5000);
