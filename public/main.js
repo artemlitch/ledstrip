1d09b7b2a3f0bcf220a294740ebfd1a3455c17a7
Artem Litchmanov <alitchmanov@wealthsimple.com>
2018-12-28 15:47:08 -0500

    use set color

--- public/main.js
+++ public/main.js
@@ -1,19 +1,24 @@
 function init() {
-  const demoColorPicker = new iro.ColorPicker("#color-picker-container", {
-    'wheelLightness': false
-  });
-  demoColorPicker.on('input:end', (color) => {
-    const {r, g, b} = color.rgb;
-    const data = {
-      red: r,
-      green: g,
-      blue: b,
-    };
-    $.post("http:/192.168.0.19:1337", JSON.stringify(data), function () {
-        console.log("success");
-      },
-      "json"
-    ).done(()=> { console.log("sucesss")})
+  $.get("/currentLightVal", (data) => {
+    console.log(data);
+    const demoColorPicker = new iro.ColorPicker("#color-picker-container", {
+      'wheelLightness': false,
+
+    });
+    demoColorPicker.color.rgb = {r: data.red, g: data.green, b: data.blue};
+    demoColorPicker.on('input:end', (color) => {
+      const {r, g, b} = color.rgb;
+      const data = {
+        red: r,
+        green: g,
+        blue: b,
+      };
+      $.post("/", JSON.stringify(data), function () {
+          console.log("success");
+        },
+        "json"
+      ).done(()=> { console.log("sucesss")})
+    })
   })
 };
 
