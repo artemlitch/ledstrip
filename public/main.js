809a19911e3bd2f1c715a004198b8943bd36163b
Artem Litchmanov <alitchmanov@wealthsimple.com>
2018-12-28 16:17:36 -0500

    new polling

--- public/main.js
+++ public/main.js
@@ -1,3 +1,30 @@
+function throttle(func, delay) { // allows [func] to run once every [delay] ms
+  var func = func.bind(func),
+    last = Date.now();
+  return function (color) {
+    if (Date.now() - last > delay) {
+      func(color);
+      last = Date.now();
+    }
+  }
+}
+
+const sendChangeColor = (color) => {
+  const {r, g, b} = color.rgb;
+  const data = {
+    red: r,
+    green: g,
+    blue: b,
+  };
+  $.post("/", JSON.stringify(data), function () {
+      console.log("success");
+    },
+    "json"
+  ).done(() => {
+    console.log("sucesss")
+  })
+};
+
 function init() {
   $.get("/currentLightVal", (data) => {
     console.log(data);
@@ -6,19 +33,7 @@ function init() {
 
     });
     demoColorPicker.color.rgb = {r: data.red, g: data.green, b: data.blue};
-    demoColorPicker.on('input:end', (color) => {
-      const {r, g, b} = color.rgb;
-      const data = {
-        red: r,
-        green: g,
-        blue: b,
-      };
-      $.post("/", JSON.stringify(data), function () {
-          console.log("success");
-        },
-        "json"
-      ).done(()=> { console.log("sucesss")})
-    })
+    demoColorPicker.on('color:change', throttle(sendChangeColor, 300))
   })
 };
 
